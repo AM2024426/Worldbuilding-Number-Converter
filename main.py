626ca19a55f1abaeebd9ac0c9ffdf27023bb6889
AM2024426 <168240395+AM2024426@users.noreply.github.com>
2026-05-08 21:29:25 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,12 +6,16 @@ constfactor2=11
 constfactor3=111
 octal = oct(decimal) #convert base 10 to base 8 (octal)
 octalval=int((octal[2:])) # remove 1st 2 characters (not needed)
+#bypass #s 0-8
+if (decimal<=8):
+    print(decimal)
+else:
 
-if (octalval>=100):  #if greater than 100, add 111
+  if (octalval>=100):  #if greater than 100, add 111
     octaladdedval= constfactor3 + octalval
     print(octaladdedval)
-else:
-    if (decimal<10):  #if greater than 10, add 11, less than 10, add 1
+  else:
+    if (octalval<10):  #if greater than 10, add 11, less than 10, add 1
        octaladdedval= constfactor + octalval
        print(octaladdedval)
     else: 
